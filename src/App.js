8ee63e0bbb93290aecc0a8c6f7f81832bd024aa8
aryan <aryanmahalle78924@gmail.com>
2022-08-05 11:20:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Images from "./Images";
 
-function App() {
+const App = () => {
+  const [image, setImage] = useState(Images[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Image Gallery</h2>
+      <h4>Rolls Royce Series</h4>
+      <img src={image} alt="" className="selected" />
+      <div className="imgContainer">
+        {Images.map((image) => {
+          return (
+            <img src={image} className="imgs" onClick={() => setImage(image)} />
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Images.js
@@ -0,0 +1,17 @@
+const Images = [
+  "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2020/09/02191145/2021-rolls-royce-ghost-details.jpg",
+  "https://www.drivespark.com/images/2022-05/2022-rolls-royce-phantom-10.jpg",
+  "https://img.onmanorama.com/content/dam/mm/en/business/autos/images/2020/3/12/golden-rolls-royce-phantom.jpg.transform/schema-16x9/image.jpg",
+  "https://www.rolls-roycemotorcars.com/content/dam/rrmc/marketUK/rollsroycemotorcars_com/1-0-home/page-properties/boat-tail-the-next-chapter/09_CAROUSEL_DESKTOP.jpg/jcr:content/renditions/original",
+  "https://media.istockphoto.com/photos/rollsroyce-phantom-parked-in-coventry-town-centre-picture-id1034555532?k=20&m=1034555532&s=612x612&w=0&h=65H5P5dT6-wJ99jGUlfHDnx7pSeeQHLunT555yuTG-Y=",
+  "https://i.ytimg.com/vi/ZEY3ZvObmzU/maxresdefault.jpg",
+  "https://i.pinimg.com/originals/47/62/f4/4762f4e1103afbbb89ae9baead9ab7b8.jpg",
+  "https://c.files.bbci.co.uk/B982/production/_114209474_rollsroyce_reuters.jpg",
+
+  "https://image.cnbcfm.com/api/v1/image/104499164-6_JL81019_1mostexpensiverollsroyce.jpg?v=1529475238&w=1920&h=1080",
+  "https://i.pinimg.com/originals/37/98/2e/37982e7f95fbccead48ca211d2876dbb.jpg",
+  "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iHfONtVdE.A8/v0/-1x-1.jpg",
+  "https://img.jamesedition.com/listing_images/2022/07/28/08/01/16/29a1849e-6920-4a98-b56e-853114a19b76/je/600x354xc.jpg",
+];
+
+export default Images;
